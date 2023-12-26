de861eb09f58d4d37243f9a26d25eda8f9f9e08d
Peylics <lisichen_2901@outlook.com>
2023-12-26 15:08:04 +0800

    Added an input box for entering user's chatbot API

--- app/src/main/java/com/example/curricularica/SettingsActivity.java
+++ app/src/main/java/com/example/curricularica/SettingsActivity.java
@@ -2,17 +2,20 @@ package com.example.curricularica;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ListView;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    EditText apiKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,36 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
         setSupportActionBar(myToolbar);
 
-        findViewById(R.id.button_import).setOnClickListener(v -> {
+        apiKey = findViewById(R.id.user_api);
 
-        });
+        SharedPreferences sharedPreferences = getSharedPreferences("APIKeyData", MODE_PRIVATE);
+        apiKey.setText(sharedPreferences.getString("apiKey", ""));
+
+//        findViewById(R.id.button_import).setOnClickListener(v -> {
+//
+//        });
 
         findViewById(R.id.button_create).setOnClickListener(v -> {
 
         });
 
-        findViewById(R.id.button_switch).setOnClickListener(v -> {
-
-        });
+//        findViewById(R.id.button_switch).setOnClickListener(v -> {
+//
+//        });
 
     }
+
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Use SharedPreferences to preserve input data in time
+        SharedPreferences sharedPreferences = getSharedPreferences("APIKeyData", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // Save the input data to SharedPreferences
+        editor.putString("apiKey", apiKey.getText().toString());
+
+        editor.apply();
+    }
+
 }
